0620a0a7ed29b23e5e03c91d5ae6612fa2e85efd
David Browning <djbrowning@gmail.com>
2011-04-16 15:56:27 -0400

    Added more of our portfolio pages content and images.

---
+++ public/javascripts/application.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	$('.tooltipped').tipsy({gravity:'n'});
+});
--- twoguys.rb
+++ twoguys.rb
@@ -7,20 +7,64 @@ get '/' do
   haml :index
 end
 
-get '/design' do
-  @design = true
-  haml :design
+get '/portfolio' do
+  @portfolio = true
+  haml :portfolio
 end
 
-get '/build' do
-  @build = true
-  haml :build
+get '/contact' do
+  @contact = true
+  haml :contact
 end
 
-get '/deploy' do
-  @deploy = true
-  haml :deploy
+# PROJECTS
+
+get '/portfolio/fastcustomer' do
+  @portfolio = true
+  haml :fastcustomer
 end
+get '/portfolio/remotejobs' do
+  @portfolio = true
+  haml :remotejobs
+end
+get '/portfolio/maxrvu' do
+  @portfolio = true
+  haml :maxrvu
+end
+get '/portfolio/crashbucket' do
+  @portfolio = true
+  haml :crashbucket
+end
+get '/portfolio/appmarkit' do
+  @portfolio = true
+  haml :appmarkit
+end
+get '/portfolio/nezumi' do
+  @portfolio = true
+  haml :nezumi
+end
+get '/portfolio/publishur' do
+  @portfolio = true
+  haml :publishur
+end
+get '/portfolio/wecreateideas' do
+  @portfolio = true
+  haml :wecreateideas
+end
+get '/portfolio/schoolyardstewards' do
+  @portfolio = true
+  haml :schoolyardstewards
+end
+get '/portfolio/openbrews' do
+  @portfolio = true
+  haml :openbrews
+end
+get '/portfolio/fightklub' do
+  @portfolio = true
+  haml :fightklub
+end
+
+# STYLESHEETS
 
 get '/stylesheets/reset.css' do
   content_type 'text/css', :charset => 'utf-8'
