1ecc092ffbbdc0924b6c25f2d17c44985b4a2c2c
David Browning <djbrowning@gmail.com>
2012-12-21 10:24:45 -0500

    Removed compass.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
-source :gemcutter
+source "http://rubygems.org"
 
 gem 'sinatra'
 gem 'haml'
 gem 'sass'
-gem 'compass'
--- twoguys.rb
+++ twoguys.rb
@@ -2,7 +2,6 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'sass'
-require 'compass'
 
 configure do
   set :haml, { :format => :html5 }
